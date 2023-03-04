4770e67d32e8872156a748d33a2c4748c955a4cb
Panjialfdn10 <m.panjialfianudin@gmail.com>
2023-03-04 21:36:10 +0700

    initial commit

--- app/src/main/java/com/example/ktourskotlin/main/LoginActivity.kt
+++ app/src/main/java/com/example/ktourskotlin/main/LoginActivity.kt
@@ -7,12 +7,12 @@ import com.example.ktourskotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 
 
