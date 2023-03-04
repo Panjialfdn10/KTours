e696a7c80a79a643364d2b43d58a86c7a06e9216
Panjialfdn10 <m.panjialfianudin@gmail.com>
2023-03-04 21:33:06 +0700

    initial commit

---
+++ app/src/main/java/com/example/ktourskotlin/main/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ktourskotlin.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ktourskotlin.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
